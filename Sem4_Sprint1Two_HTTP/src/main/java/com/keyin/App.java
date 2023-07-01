64317a58da2925b95235949c4dc60dac40b20a71
Darla Ward <105027556+darlamward@users.noreply.github.com>
2023-07-01 12:53:41 -0230

    changes to App.java

--- Sem4_Sprint1Two_HTTP/src/main/java/com/keyin/App.java
+++ Sem4_Sprint1Two_HTTP/src/main/java/com/keyin/App.java
@@ -3,7 +3,7 @@ package com.keyin;
 import com.keyin.http.cli.HTTPRestCLIApplication;
 import com.keyin.http.client.RESTClient;
 
-public class App {
+public class ClientRunner {
     public static void main(String[] args) {
 
         // make a switch statement here to take in commandline arguments ***********************************************
@@ -12,77 +12,6 @@ public class App {
 
         cliApp.setRestClient(new RESTClient());
 
-        /*
-
-        System.out.println();
-
-        cliApp.generateAircraftReport();
-
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println();
-
-        cliApp.generateAirportReport();
-
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println();
-
-        cliApp.generateCityReport();
-
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println();
-
-        cliApp.generatePassengerReport();
-
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println();
-
-        // this also show the relationship, aircraft to airports
-        cliApp.getAircraftByIdReport(1);
-
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println();
-
-        cliApp.getAirportByIdReport(1);
-
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println();
-
-        // relationship to show all airports in a city
-        cliApp.getAirportsByCityIdReport(1);
-
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println();
-
-        cliApp.getCityByIdReport(2);
-
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println();
-
-        cliApp.getPassengerByIdReport(2);
-
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println();
-
-        // relationship to show aircraft's a passenger has been on
-        cliApp.getAircraftPassengerBeenOnReport(2);
-
-        System.out.println();
-        System.out.println("-----------------------------------------------------------------------------------------");
-        System.out.println();
-
-        // relationship to show airports a passenger has visited
-        cliApp.getAirportPassengerVisitedReport(1);
-
-         */
 
 
         if (args.length > 0) {
@@ -130,17 +59,17 @@ public class App {
                     cliApp.getAirportPassengerVisitedReport(Integer.parseInt(args[1]));
                     break;
 
-                // History commands
-                case "history":
+                // Browser commands
+                case "browser":
                     cliApp.getBrowserPeekReport();
                     break;
-                case "allHistory":
+                case "allBrowser":
                     cliApp.getBrowserDataReport();
                     break;
-                case "undoHistory":
+                case "undoBrowser":
                     cliApp.getUndoBrowser();
                     break;
-                case "redoHistory":
+                case "redoBrowser":
                     cliApp.getRedoBrowser();
                     break;
                 default:
